aa155d502edc665d9a32215c072d0a40446ef5f4
Shradha Nair <70503824+sh7-git-user@users.noreply.github.com>
2026-03-06 16:00:37 +0530

    Add face authentication function using Cordova exec

--- www/faceauth.js
+++ www/faceauth.js
@@ -1,1 +1,12 @@
+var exec = require('cordova/exec');
 
+exports.faceAuth = function (salt, success, error) {
+
+    exec(
+        success,
+        error,
+        "FaceAuthPlugin",
+        "faceAuth",
+        [salt]
+    );
+};
