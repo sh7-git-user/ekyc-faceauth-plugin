91f6f34c9d8e6181d5c8ec435045ddea22962dae
Shradha Nair <70503824+sh7-git-user@users.noreply.github.com>
2026-03-07 12:37:54 +0530

    Refactor FaceAuthPlugin to improve action handling

--- src/android/FaceAuthPlugin.java
+++ src/android/FaceAuthPlugin.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.ResultReceiver;
+import android.provider.Settings;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -20,103 +21,125 @@ public class FaceAuthPlugin extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
-        if (action.equals("faceAuth")) {
-            android.util.Log.d("FaceAuthPlugin", "FaceAuth action triggered");
-            Activity activity = cordova.getActivity();
-            String salt = args.getString(0);
-
-            activity.runOnUiThread(() -> {
-
-                try {
-
-                    String cred = "{\"CredAllowed\":[{\"type\":\"BIOMETRIC\",\"subtype\":\"FACE_AUTH\"}]}";
-
-                    String keyCode = "EKYC";
-                    String langPref = "en_US";
-
-                    CLServices.initService(activity, new ServiceConnectionStatusNotifier() {
-
-                        @Override
-                        public void serviceConnected(CLServices services) {
-                              android.util.Log.d("FaceAuthPlugin", "RD service connected");   
-                            // Store service locally instead of Constant.clServices
-                            CLServices clServices = services;
-                       String txnId = String.valueOf(System.currentTimeMillis());
-
-String deviceId = android.provider.Settings.Secure.getString(
-        activity.getContentResolver(),
-        android.provider.Settings.Secure.ANDROID_ID
-    );
-                            CLRemoteResultReceiver receiver =
-                                    new CLRemoteResultReceiver(new ResultReceiver(new Handler()) {
-
-                                        @Override
-                                        protected void onReceiveResult(int resultCode, Bundle resultData) {
-                                           android.util.Log.d("FaceAuthPlugin", "RD Result Bundle: " + resultData);
-                                            if (resultData != null) {
-
-    String result = "";
-
-    if (resultData.containsKey("PID_DATA")) {
-        result = resultData.getString("PID_DATA");
-    } 
-    else if (resultData.containsKey("PID_DATA_XML")) {
-        result = resultData.getString("PID_DATA_XML");
-    }
-    else if (resultData.containsKey("RESULT")) {
-        result = resultData.getString("RESULT");
-    }
-    else {
-        result = resultData.toString();
-    }
-
-    android.util.Log.d("FaceAuthPlugin", "FaceAuth result: " + result);
-
-    callbackContext.success(result);
-
-} else {
-
-    callbackContext.error("Empty result from FaceAuth");
-
-}
-
-                                        }
-
-                                    });
-
-                            clServices.getCredential(
-        keyCode,
-        txnId,
-        cred,
-        "user@upi",
-        salt,
-        deviceId,
-        "ANDROID",
-        langPref,
-        receiver
-);
-                        }
-
-                        @Override
-                        public void serviceDisconnected() {
-
-                            callbackContext.error("SDK service disconnected");
-
-                        }
-
-                    });
-
-                } catch (Exception e) {
-
-                    callbackContext.error(e.getMessage());
-
-                }
-
-            });
-
-            return true;
+        if (!action.equals("faceAuth")) {
+            return false;
         }
 
-        return false;
+        android.util.Log.d("FaceAuthPlugin", "FaceAuth action triggered");
+
+        Activity activity = cordova.getActivity();
+        String salt = args.getString(0);
+
+        activity.runOnUiThread(() -> {
+
+            try {
+
+                String cred = "{\"CredAllowed\":[{\"type\":\"BIOMETRIC\",\"subtype\":\"FACE_AUTH\"}]}";
+                String keyCode = "EKYC";
+                String langPref = "en_US";
+
+                String txnId = String.valueOf(System.currentTimeMillis());
+
+                String deviceId = Settings.Secure.getString(
+                        activity.getContentResolver(),
+                        Settings.Secure.ANDROID_ID
+                );
+
+                android.util.Log.d("FaceAuthPlugin", "Transaction ID: " + txnId);
+                android.util.Log.d("FaceAuthPlugin", "Device ID: " + deviceId);
+
+                CLServices.initService(activity, new ServiceConnectionStatusNotifier() {
+
+                    @Override
+                    public void serviceConnected(CLServices services) {
+
+                        android.util.Log.d("FaceAuthPlugin", "RD service connected");
+
+                        CLServices clServices = services;
+
+                        CLRemoteResultReceiver receiver =
+                                new CLRemoteResultReceiver(new ResultReceiver(new Handler()) {
+
+                                    @Override
+                                    protected void onReceiveResult(int resultCode, Bundle resultData) {
+
+                                        android.util.Log.d("FaceAuthPlugin", "RD Result Code: " + resultCode);
+                                        android.util.Log.d("FaceAuthPlugin", "RD Result Bundle: " + resultData);
+
+                                        if (resultData != null) {
+
+                                            String result;
+
+                                            if (resultData.containsKey("PID_DATA")) {
+                                                result = resultData.getString("PID_DATA");
+                                            }
+                                            else if (resultData.containsKey("PID_DATA_XML")) {
+                                                result = resultData.getString("PID_DATA_XML");
+                                            }
+                                            else if (resultData.containsKey("RESULT")) {
+                                                result = resultData.getString("RESULT");
+                                            }
+                                            else {
+                                                result = resultData.toString();
+                                            }
+
+                                            android.util.Log.d("FaceAuthPlugin", "FaceAuth result: " + result);
+
+                                            callbackContext.success(result);
+
+                                        } else {
+
+                                            android.util.Log.e("FaceAuthPlugin", "Empty result from FaceAuth");
+                                            callbackContext.error("Empty result from FaceAuth");
+
+                                        }
+                                    }
+
+                                });
+
+                        try {
+
+                            android.util.Log.d("FaceAuthPlugin", "Calling getCredential...");
+
+                            clServices.getCredential(
+                                    keyCode,
+                                    txnId,
+                                    cred,
+                                    "user@upi",
+                                    salt,
+                                    deviceId,
+                                    "ANDROID",
+                                    langPref,
+                                    receiver
+                            );
+
+                        } catch (Exception e) {
+
+                            android.util.Log.e("FaceAuthPlugin", "getCredential failed: " + e.getMessage());
+                            callbackContext.error(e.getMessage());
+
+                        }
+                    }
+
+                    @Override
+                    public void serviceDisconnected() {
+
+                        android.util.Log.e("FaceAuthPlugin", "SDK service disconnected");
+                        callbackContext.error("SDK service disconnected");
+
+                    }
+
+                });
+
+            } catch (Exception e) {
+
+                android.util.Log.e("FaceAuthPlugin", "Exception: " + e.getMessage());
+                callbackContext.error(e.getMessage());
+
+            }
+
+        });
+
+        return true;
     }
 }
