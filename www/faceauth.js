ed1245e9dd824dec378cdf20429b4c937000e934
Shradha Nair <70503824+sh7-git-user@users.noreply.github.com>
2026-03-07 12:39:51 +0530

    Require salt for face authentication
    Added validation to check if salt is provided before executing face authentication.

--- www/faceauth.js
+++ www/faceauth.js
@@ -2,6 +2,13 @@ var exec = require('cordova/exec');
 
 exports.faceAuth = function (salt, success, error) {
 
+    if (!salt) {
+        if (error) {
+            error("Salt is required");
+        }
+        return;
+    }
+
     exec(
         success,
         error,
